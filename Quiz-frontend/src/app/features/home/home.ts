addee1ba006e3779d94a765105df0ba0f96920e1
Trashung <phuchung2601@gmail.com>
2026-04-05 19:19:03 +0700

    Home

--- Quiz-frontend/src/app/features/home/home.ts
+++ Quiz-frontend/src/app/features/home/home.ts
@@ -1,26 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class Home {
-  // Hàm xử lý tự động nhảy ô
-  moveFocus(event: any, nextElement: HTMLInputElement | null, prevElement: HTMLInputElement | null) {
-    const input = event.target as HTMLInputElement;
-    const length = input.value.length;
+  joinPin: string = '';
 
-    // Nếu đã gõ xong 1 chữ số và có ô tiếp theo -> Nhảy tới
-    if (length === 1 && nextElement) {
-      nextElement.focus();
-    }
-
-    // Nếu nhấn Backspace khi ô đang trống và có ô phía trước -> Quay lại
-    if (event.key === 'Backspace' && length === 0 && prevElement) {
-      prevElement.focus();
-    }
+  onPinInput(event: any) {
+    this.joinPin = this.joinPin.toUpperCase();
   }
 }
